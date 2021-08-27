51fd32147d247bbdf067280b1d6d142675e3575b
pxltr <tymon.zaniewski.pl@gmail.com>
2021-08-27 17:14:17 +0200

    .

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
+
 const config = require('./config.json')
+// const command = require('./command')
 
 client.on('ready', () => {
 	console.log('CLIENT IS READY')
+
+	// command(client, 'ping', message => {
+	// 	message.channel.send('Pong')
+	// })
 })
 
 client.login(config.token)
